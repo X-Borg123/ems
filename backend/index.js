828e4d9409e9e127a36b0a9a2f29918324447b46
Pyae Sone Phyo <pyaesonephyo@evercomm.io>
2026-07-04 10:35:51 +0630

    feat: api health for cron job

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static(uploadsDir));
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true, message: "Server is running" });
+});
+
 app.use("/api/auth", authRouter);
 app.use("/api/department", departmentRouter);
 app.use("/api/employee", employeeRouter);
